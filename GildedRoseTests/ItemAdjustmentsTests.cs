7070cade83250a012a1fc65e2600ed61d9e13c92
Tom Ogle <togle@foxguardsolutions.com>
2020-09-02 19:03:41 -0400

    Implemented inventory Sell In and Quality refactoring calculations
    -Created seperate class for updating item values
    -Added tests for item calculations and updates

---
+++ GildedRoseTests/ItemAdjustmentsTests.cs
@@ -0,0 +1,268 @@
+﻿using System.Collections.Generic;
+using GildedRoseApp;
+using static GildedRoseApp.InventoryItem;
+using NUnit.Framework;
+using System.Linq;
+
+namespace GildedRoseTests
+{
+    [TestFixture]
+    
+    public class ItemAdjustmentsTests
+    {
+        private IEnumerable<InventoryItem> _itemsNotExpired;
+        private IEnumerable<InventoryItem> _itemsExpired;
+        private IEnumerable<InventoryItem> _backstagePassTenDay;
+        private IEnumerable<InventoryItem> _backstagePassFiveDay;
+
+        [SetUp]
+        public void Init()
+        {
+            _itemsNotExpired = new List<InventoryItem> {
+                new InventoryItem { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20, Category = CategoryList.Standard },
+                new InventoryItem { Name = "Aged Brie", SellIn = 2, Quality = 0, Category = CategoryList.AgedBrie },
+                new InventoryItem { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7, Category = CategoryList.Standard },
+                new InventoryItem { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80, Category = CategoryList.Sulfuras },
+                new InventoryItem { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80, Category = CategoryList.Sulfuras },
+                new InventoryItem { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20, Category = CategoryList.BackstagePasses },
+                new InventoryItem { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49, Category = CategoryList.BackstagePasses },
+                new InventoryItem { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 49, Category = CategoryList.BackstagePasses },
+                new InventoryItem { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6, Category = CategoryList.Conjured },
+            };
+
+            _itemsExpired = new List<InventoryItem> {
+                new InventoryItem { Name = "+5 Dexterity Vest", SellIn = -1, Quality = 8, Category = CategoryList.Standard },
+                new InventoryItem { Name = "Aged Brie", SellIn = -9, Quality = 20, Category = CategoryList.AgedBrie },
+                new InventoryItem { Name = "Elixir of the Mongoose", SellIn = -6, Quality = 0, Category = CategoryList.Standard },
+                new InventoryItem { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80, Category = CategoryList.Sulfuras },
+                new InventoryItem { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80, Category = CategoryList.Sulfuras },
+                new InventoryItem { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -1, Quality = 0, Category = CategoryList.BackstagePasses },
+                new InventoryItem { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -10, Quality = 0, Category = CategoryList.BackstagePasses },
+                new InventoryItem { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -5, Quality = 0, Category = CategoryList.BackstagePasses },
+                new InventoryItem { Name = "Conjured Mana Cake", SellIn = -8, Quality = 0, Category = CategoryList.Conjured },
+            };
+
+            _backstagePassTenDay = new List<InventoryItem> {
+                new InventoryItem { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 25, Category = CategoryList.BackstagePasses },
+            };
+
+            _backstagePassFiveDay = new List<InventoryItem> {
+                new InventoryItem { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 35, Category = CategoryList.BackstagePasses },
+            };
+        }
+
+        [Test]
+        [TestCase(10, 20, 9, 19)]
+        public void UpdateItemValues_GivenSellInAndQualityOfStandardNonExpiredItem_ReturnUpdatedSellInAndQuality(int sellIn, int quality, int updatedSellIn, int updatedQuality)
+        {
+            var standardBeforeAdjustment = _itemsNotExpired.Single(item => item.Name == "+5 Dexterity Vest" && item.Category.Equals(CategoryList.Standard));
+            var beforeAdjustmentSellIn = standardBeforeAdjustment.SellIn;
+            var beforeAdjustmentQuality = standardBeforeAdjustment.Quality;
+
+            var itemAdjustments = new ItemAdjustments();
+            itemAdjustments.UpdateItemValues(_itemsNotExpired);
+            var standardAfterAdjustment = _itemsNotExpired.Single(item => item.Name == "+5 Dexterity Vest" && item.Category.Equals(CategoryList.Standard));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(beforeAdjustmentSellIn, Is.EqualTo(sellIn));
+                Assert.That(standardAfterAdjustment.SellIn, Is.EqualTo(updatedSellIn));
+                Assert.That(beforeAdjustmentQuality, Is.EqualTo(quality));
+                Assert.That(standardAfterAdjustment.Quality, Is.EqualTo(updatedQuality));
+            });
+        }
+
+        [Test]
+        [TestCase(2, 0, 1, 1)]
+        public void UpdateItemValues_GivenSellInAndQualityOfAgedBrieNonExpiredItem_ReturnUpdatedSellInAndQuality(int sellIn, int quality, int updatedSellIn, int updatedQuality)
+        {
+            var standardBeforeAdjustment = _itemsNotExpired.Single(item => item.Name == "Aged Brie" && item.Category.Equals(CategoryList.AgedBrie));
+            var beforeAdjustmentSellIn = standardBeforeAdjustment.SellIn;
+            var beforeAdjustmentQuality = standardBeforeAdjustment.Quality;
+
+            var itemAdjustments = new ItemAdjustments();
+            itemAdjustments.UpdateItemValues(_itemsNotExpired);
+            var standardAfterAdjustment = _itemsNotExpired.Single(item => item.Name == "Aged Brie" && item.Category.Equals(CategoryList.AgedBrie));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(beforeAdjustmentSellIn, Is.EqualTo(sellIn));
+                Assert.That(standardAfterAdjustment.SellIn, Is.EqualTo(updatedSellIn));
+                Assert.That(beforeAdjustmentQuality, Is.EqualTo(quality));
+                Assert.That(standardAfterAdjustment.Quality, Is.EqualTo(updatedQuality));
+            });
+        }
+
+        [Test]
+        [TestCase(0, 80, 0, 80)]
+        public void UpdateItemValues_GivenSellInAndQualityOfSulfurasNonExpiredItem_ReturnUpdatedSellInAndQuality(int sellIn, int quality, int updatedSellIn, int updatedQuality)
+        {
+            var standardBeforeAdjustment = _itemsNotExpired.First(item => item.Name == "Sulfuras, Hand of Ragnaros" && item.Category.Equals(CategoryList.Sulfuras));
+            var beforeAdjustmentSellIn = standardBeforeAdjustment.SellIn;
+            var beforeAdjustmentQuality = standardBeforeAdjustment.Quality;
+
+            var itemAdjustments = new ItemAdjustments();
+            itemAdjustments.UpdateItemValues(_itemsNotExpired);
+            var standardAfterAdjustment = _itemsNotExpired.First(item => item.Name == "Sulfuras, Hand of Ragnaros" && item.Category.Equals(CategoryList.Sulfuras));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(beforeAdjustmentSellIn, Is.EqualTo(sellIn));
+                Assert.That(standardAfterAdjustment.SellIn, Is.EqualTo(updatedSellIn));
+                Assert.That(beforeAdjustmentQuality, Is.EqualTo(quality));
+                Assert.That(standardAfterAdjustment.Quality, Is.EqualTo(updatedQuality));
+            });
+        }
+
+        [Test]
+        [TestCase(15, 20, 14, 21)]
+        public void UpdateItemValues_GivenSellInAndQualityOfBackstagePassesNonExpiredItem_ReturnUpdatedSellInAndQuality(int sellIn, int quality, int updatedSellIn, int updatedQuality)
+        {
+            var standardBeforeAdjustment = _itemsNotExpired.First(item => item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Category.Equals(CategoryList.BackstagePasses));
+            var beforeAdjustmentSellIn = standardBeforeAdjustment.SellIn;
+            var beforeAdjustmentQuality = standardBeforeAdjustment.Quality;
+
+            var itemAdjustments = new ItemAdjustments();
+            itemAdjustments.UpdateItemValues(_itemsNotExpired);
+            var standardAfterAdjustment = _itemsNotExpired.First(item => item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Category.Equals(CategoryList.BackstagePasses));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(beforeAdjustmentSellIn, Is.EqualTo(sellIn));
+                Assert.That(standardAfterAdjustment.SellIn, Is.EqualTo(updatedSellIn));
+                Assert.That(beforeAdjustmentQuality, Is.EqualTo(quality));
+                Assert.That(standardAfterAdjustment.Quality, Is.EqualTo(updatedQuality));
+            });
+        }
+
+        //***********************************************************************************
+
+        [Test]
+        [TestCase(-1, 8, -2, 6)]
+        public void UpdateItemValues_GivenSellInAndQualityOfStandardExpiredItem_ReturnUpdatedSellInAndQuality(int sellIn, int quality, int updatedSellIn, int updatedQuality)
+        {
+            var standardBeforeAdjustment = _itemsExpired.Single(item => item.Name == "+5 Dexterity Vest" && item.Category.Equals(CategoryList.Standard));
+            var beforeAdjustmentSellIn = standardBeforeAdjustment.SellIn;
+            var beforeAdjustmentQuality = standardBeforeAdjustment.Quality;
+
+            var itemAdjustments = new ItemAdjustments();
+            itemAdjustments.UpdateItemValues(_itemsExpired);
+            var standardAfterAdjustment = _itemsExpired.Single(item => item.Name == "+5 Dexterity Vest" && item.Category.Equals(CategoryList.Standard));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(beforeAdjustmentSellIn, Is.EqualTo(sellIn));
+                Assert.That(standardAfterAdjustment.SellIn, Is.EqualTo(updatedSellIn));
+                Assert.That(beforeAdjustmentQuality, Is.EqualTo(quality));
+                Assert.That(standardAfterAdjustment.Quality, Is.EqualTo(updatedQuality));
+            });
+        }
+
+        [Test]
+        [TestCase(-9, 20, -10, 22)]
+        public void UpdateItemValues_GivenSellInAndQualityOfAgedBrieExpiredItem_ReturnUpdatedSellInAndQuality(int sellIn, int quality, int updatedSellIn, int updatedQuality)
+        {
+            var standardBeforeAdjustment = _itemsExpired.Single(item => item.Name == "Aged Brie" && item.Category.Equals(CategoryList.AgedBrie));
+            var beforeAdjustmentSellIn = standardBeforeAdjustment.SellIn;
+            var beforeAdjustmentQuality = standardBeforeAdjustment.Quality;
+
+            var itemAdjustments = new ItemAdjustments();
+            itemAdjustments.UpdateItemValues(_itemsExpired);
+            var standardAfterAdjustment = _itemsExpired.Single(item => item.Name == "Aged Brie" && item.Category.Equals(CategoryList.AgedBrie));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(beforeAdjustmentSellIn, Is.EqualTo(sellIn));
+                Assert.That(standardAfterAdjustment.SellIn, Is.EqualTo(updatedSellIn));
+                Assert.That(beforeAdjustmentQuality, Is.EqualTo(quality));
+                Assert.That(standardAfterAdjustment.Quality, Is.EqualTo(updatedQuality));
+            });
+        }
+
+        [Test]
+        [TestCase(-1, 80, -1, 80)]
+        public void UpdateItemValues_GivenSellInAndQualityOfSulfurasExpiredItem_ReturnUpdatedSellInAndQuality(int sellIn, int quality, int updatedSellIn, int updatedQuality)
+        {
+            var standardBeforeAdjustment = _itemsExpired.First(item => item.Name == "Sulfuras, Hand of Ragnaros" && item.Category.Equals(CategoryList.Sulfuras));
+            var beforeAdjustmentSellIn = standardBeforeAdjustment.SellIn;
+            var beforeAdjustmentQuality = standardBeforeAdjustment.Quality;
+
+            var itemAdjustments = new ItemAdjustments();
+            itemAdjustments.UpdateItemValues(_itemsExpired);
+            var standardAfterAdjustment = _itemsExpired.First(item => item.Name == "Sulfuras, Hand of Ragnaros" && item.Category.Equals(CategoryList.Sulfuras));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(beforeAdjustmentSellIn, Is.EqualTo(sellIn));
+                Assert.That(standardAfterAdjustment.SellIn, Is.EqualTo(updatedSellIn));
+                Assert.That(beforeAdjustmentQuality, Is.EqualTo(quality));
+                Assert.That(standardAfterAdjustment.Quality, Is.EqualTo(updatedQuality));
+            });
+        }
+
+        [Test]
+        [TestCase(-1, 0, -2, 0)]
+        public void UpdateItemValues_GivenSellInAndQualityOfBackstagePassesExpiredItem_ReturnUpdatedSellInAndQuality(int sellIn, int quality, int updatedSellIn, int updatedQuality)
+        {
+            var standardBeforeAdjustment = _itemsExpired.First(item => item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Category.Equals(CategoryList.BackstagePasses));
+            var beforeAdjustmentSellIn = standardBeforeAdjustment.SellIn;
+            var beforeAdjustmentQuality = standardBeforeAdjustment.Quality;
+
+            var itemAdjustments = new ItemAdjustments();
+            itemAdjustments.UpdateItemValues(_itemsExpired);
+            var standardAfterAdjustment = _itemsExpired.First(item => item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Category.Equals(CategoryList.BackstagePasses));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(beforeAdjustmentSellIn, Is.EqualTo(sellIn));
+                Assert.That(standardAfterAdjustment.SellIn, Is.EqualTo(updatedSellIn));
+                Assert.That(beforeAdjustmentQuality, Is.EqualTo(quality));
+                Assert.That(standardAfterAdjustment.Quality, Is.EqualTo(updatedQuality));
+            });
+        }
+
+        // *****************************************************************************
+
+        [Test]
+        [TestCase(10, 25, 9, 27)]
+        public void UpdateItemValues_GivenSellInAndQualityOfBackstagePassesTenDaysBeforeShow_ReturnUpdatedSellInAndQuality(int sellIn, int quality, int updatedSellIn, int updatedQuality)
+        {
+            var standardBeforeAdjustment = _backstagePassTenDay.First(item => item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Category.Equals(CategoryList.BackstagePasses));
+            var beforeAdjustmentSellIn = standardBeforeAdjustment.SellIn;
+            var beforeAdjustmentQuality = standardBeforeAdjustment.Quality;
+
+            var itemAdjustments = new ItemAdjustments();
+            itemAdjustments.UpdateItemValues(_backstagePassTenDay);
+            var standardAfterAdjustment = _backstagePassTenDay.First(item => item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Category.Equals(CategoryList.BackstagePasses));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(beforeAdjustmentSellIn, Is.EqualTo(sellIn));
+                Assert.That(standardAfterAdjustment.SellIn, Is.EqualTo(updatedSellIn));
+                Assert.That(beforeAdjustmentQuality, Is.EqualTo(quality));
+                Assert.That(standardAfterAdjustment.Quality, Is.EqualTo(updatedQuality));
+            });
+        }
+
+        [Test]
+        [TestCase(5, 35, 4, 38)]
+        public void UpdateItemValues_GivenSellInAndQualityOfBackstagePassesFiveDaysBeforeShow_ReturnUpdatedSellInAndQuality(int sellIn, int quality, int updatedSellIn, int updatedQuality)
+        {
+            var standardBeforeAdjustment = _backstagePassFiveDay.First(item => item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Category.Equals(CategoryList.BackstagePasses));
+            var beforeAdjustmentSellIn = standardBeforeAdjustment.SellIn;
+            var beforeAdjustmentQuality = standardBeforeAdjustment.Quality;
+
+            var itemAdjustments = new ItemAdjustments();
+            itemAdjustments.UpdateItemValues(_backstagePassFiveDay);
+            var standardAfterAdjustment = _backstagePassFiveDay.First(item => item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Category.Equals(CategoryList.BackstagePasses));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(beforeAdjustmentSellIn, Is.EqualTo(sellIn));
+                Assert.That(standardAfterAdjustment.SellIn, Is.EqualTo(updatedSellIn));
+                Assert.That(beforeAdjustmentQuality, Is.EqualTo(quality));
+                Assert.That(standardAfterAdjustment.Quality, Is.EqualTo(updatedQuality));
+            });
+        }
+    }
+}
---
+++ csharp/ItemAdjustments.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using static GildedRoseApp.InventoryItem;
+
+namespace GildedRoseApp
+{
+    public class ItemAdjustments
+    {
+        private const int MaxQuality = 50;
+        private const int MinQuality = 0;
+        private const int MinAgedDate = 0;
+        private const int NormalQualityIncrease = 1;
+        private const int DoubleQualityIncrease = 2;
+        private const int TripleQualityIncrease = 3;
+        private const int NormalQualityDecrease = -1;
+        private const int DoubleQualityDecrease = -2;
+
+        public void UpdateItemValues(IEnumerable<InventoryItem> items)
+        {
+            foreach (var item in items)
+            {
+                if (!item.Category.Equals(CategoryList.Sulfuras)) { item.SellIn -= 1; }
+
+                switch (item.Category)
+                {
+                    case CategoryList.AgedBrie:
+                        UpdateAgedBrie(item);
+                        break;
+                    case CategoryList.BackstagePasses:
+                        UpdateBackStagePass(item);
+                        break;
+                    case CategoryList.Sulfuras:
+                        UpdateSulfuras(item);
+                        break;
+                    case CategoryList.Conjured:
+                        UpdateConjured(item);
+                        break;
+                    default:
+                        UpdateStandard(item);
+                        break;
+                }
+            }
+        }
+
+        private static int GetQualityIncrease(int quality, int adjustment)
+        {
+            return (quality + adjustment >= MaxQuality) ? MaxQuality : quality += adjustment;
+        }
+
+        private static int GetQualityDecrease(int quality, int adjustment)
+        {
+            return (quality + adjustment <= MinQuality) ? MinQuality : quality += adjustment;
+        }
+
+        private static bool IsPastAgedDate(int sellIn) => sellIn < MinAgedDate ? true : false;
+
+        private static void UpdateAgedBrie(InventoryItem item)
+        {
+            var currentQuality = item.Quality;
+            item.Quality = IsPastAgedDate(item.SellIn) ? GetQualityIncrease(currentQuality, DoubleQualityIncrease) : GetQualityIncrease(currentQuality, NormalQualityIncrease);
+        }
+
+        private static void UpdateBackStagePass(InventoryItem item)
+        {
+            item.Quality = !IsPastAgedDate(item.SellIn) ? GetBackStagePassQuality(item) : MinQuality;
+        }
+
+        private static int GetBackStagePassQuality(InventoryItem item)
+        {
+            var currentQuality = item.Quality;
+
+            if (item.SellIn < 5)
+            {
+                return GetQualityIncrease(currentQuality, TripleQualityIncrease);
+            }
+            else if (item.SellIn < 10)
+            {
+                return GetQualityIncrease(currentQuality, DoubleQualityIncrease);
+            }
+            else
+            {
+                return GetQualityIncrease(currentQuality, NormalQualityIncrease);
+            }
+        }
+
+        private static void UpdateSulfuras(InventoryItem item) { return; }
+
+        private static void UpdateStandard(InventoryItem item)
+        {
+            var currentQuality = item.Quality;
+            item.Quality = GetQualityDecrease(currentQuality, item.SellIn < 0 ? DoubleQualityDecrease : NormalQualityDecrease);
+        }
+
+        private static void UpdateConjured(InventoryItem item)
+        {
+            var currentQuality = item.Quality;
+            item.Quality = GetQualityDecrease(currentQuality, item.SellIn < 0 ? DoubleQualityDecrease : NormalQualityDecrease);
+        }
+    }
+}
